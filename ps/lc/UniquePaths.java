bd037e1ac31a98c00661e35204e3b4d5607402a1
Avinash Chandwani <avinashc.1602@gmail.com>
2021-09-04 11:00:31 +0530

    Use StringBuilder

--- ps/lc/UniquePaths.java
+++ ps/lc/UniquePaths.java
@@ -3,28 +3,28 @@ import java.util.List;
 
 public class UniquePaths {
 
-    public List<String> getAllPaths(int N) {
+   public List<String> getAllPaths(int N) {
         List<String> allPaths = new ArrayList<>();
-        traverseAllPaths(N, 0, 0, "", 0, 0, allPaths);
+        traverseAllPaths(N, 0, 0, new StringBuilder(), 0, 0, allPaths);
         return allPaths;
     }
 
     private void traverseAllPaths(int N, int x, int y,
-                                  String pathSoFar,
+                                  StringBuilder pathSoFar,
                                   int previousX, int previousY,
                                   List<String> allPaths) {
         if (x == N || y == N) return;
         if (x > 0 && previousX == x - 1) {
-            pathSoFar += "R";
+            pathSoFar.append("R");
         }
         if (y > 0 && previousY == y - 1) {
-            pathSoFar += "D";
+            pathSoFar.append("D");
         }
         if (x == N - 1 && y == N - 1) {
-            allPaths.add(pathSoFar);
+            allPaths.add(new String(pathSoFar.toString()));
             return;
         }
-        traverseAllPaths(N, x + 1, y, pathSoFar, x, y, allPaths);
-        traverseAllPaths(N, x, y + 1, pathSoFar, x, y, allPaths);
+        traverseAllPaths(N, x + 1, y, new StringBuilder(pathSoFar), x, y, allPaths);
+        traverseAllPaths(N, x, y + 1, new StringBuilder(pathSoFar), x, y, allPaths);
     }
 }
