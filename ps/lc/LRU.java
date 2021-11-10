60558d0e388114ec2cdce4e4287fdf0bcf19f529
Avinash Chandwani <avinashc.1602@gmail.com>
2021-11-10 10:54:08 +0530

    Update LRU.java

--- ps/lc/LRU.java
+++ ps/lc/LRU.java
@@ -1,84 +1,114 @@
-class LRUCache {
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class LRUCache {
+
+    private Map<Integer, DoublyNode> cacheMap = null;
     private int capacity;
-    private Map<Integer, DoublyNode> cacheMap;
-    private DoublyNode head = null, tail = null;
+    private DoublyNode head, tail;
+
     public LRUCache(int capacity) {
+        this.cacheMap = new HashMap<>();
         this.capacity = capacity;
-        cacheMap = new HashMap<>();
-        head = new DoublyNode();
-        tail = new DoublyNode();
-        head.next = tail;
-        tail.previous = head;
+        this.head = new DoublyNode();
+        this.tail = new DoublyNode();
+        this.head.setNextNode(this.tail);
+        this.tail.setPreviousNode(this.head);
     }
-    
-    public int get(int key) {
-        if(capacity <=0 || !cacheMap.containsKey(key)) {
-            return -1;
-        }
-        DoublyNode node = cacheMap.get(key);
-        moveNodeToFront(node);
-        return node.value;
-    }
-    
+
     public void put(int key, int value) {
-        if(capacity <=0) {
-            return;
-        }
-        if(cacheMap.containsKey(key)) {
-            DoublyNode node = cacheMap.get(key);
-            node.value = value;
-            moveNodeToFront(node);
-        } else {
-            DoublyNode node = new DoublyNode(key, value);
-            cacheMap.put(key, node);
-            addNewNode(node);
-            if(cacheMap.size() > capacity) {
-                int removeKey = removeLRUNode();
-                cacheMap.remove(removeKey);
+        if (cacheMap.containsKey(key)) { //update scenario
+            DoublyNode valueNode = cacheMap.get(key);
+            valueNode.setValue(value);
+            moveNodeToFront(valueNode);
+        } else {// new key scenario
+            DoublyNode valueNode = new DoublyNode(key, value);
+            cacheMap.put(key, valueNode);
+            addNodeToFront(valueNode);
+            if(cacheMap.size() > this.capacity) {
+                int removalKey = removeLRUNode();
+                cacheMap.remove(removalKey);
             }
         }
     }
-    
-    private void moveNodeToFront(DoublyNode node) {
-        node.next.previous = node.previous;
-        node.previous.next = node.next;
-        node.next = head.next;
-        node.next.previous = node;
-        node.previous = head;
-        head.next = node;
-    }
-    
-    private void addNewNode(DoublyNode node) {
-        node.next = head.next;
-        node.next.previous = node;
-        node.previous = head;
-        head.next = node;
-    }
-    
-    private int removeLRUNode() {
-        int key = -1;
-        if(tail.previous != head) {
-            DoublyNode lruNode = tail.previous;
-            key = lruNode.key;
-            lruNode.previous.next= tail;
-            tail.previous = lruNode.previous;
+
+    public int get(int key) {
+        if (!cacheMap.containsKey(key)) {
+            return -1;
         }
-        return key;
+        DoublyNode valueNode = cacheMap.get(key);
+        moveNodeToFront(valueNode);
+        return valueNode.getValue();
+    }
+
+    private void addNodeToFront(DoublyNode node) {
+        node.setNextNode(head.getNextNode());
+        head.getNextNode().setPreviousNode(node);
+        head.setNextNode(node);
+        node.setPreviousNode(head);
+    }
+
+    private int removeLRUNode() {
+        DoublyNode lruNode = this.tail.getPreviousNode();
+        removeNode(lruNode);
+        return lruNode.getKey();
+    }
+
+    private void removeNode(DoublyNode node) {
+        node.getNextNode().setPreviousNode(node.getPreviousNode());
+        node.getPreviousNode().setNextNode(node.getNextNode());
+    }
+
+    private void moveNodeToFront(DoublyNode node) {
+        removeNode(node);
+        addNodeToFront(node);
     }
 }
 
 class DoublyNode {
-    int key;
-    int value;
-    DoublyNode previous;
-    DoublyNode next;
-    
+    private int key;
+    private int value;
+    private DoublyNode previousNode;
+    private DoublyNode nextNode;
+
     public DoublyNode() {
-        
     }
+
     public DoublyNode(int key, int value) {
         this.key = key;
         this.value = value;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public DoublyNode getPreviousNode() {
+        return previousNode;
+    }
+
+    public void setPreviousNode(DoublyNode previousNode) {
+        this.previousNode = previousNode;
+    }
+
+    public DoublyNode getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(DoublyNode nextNode) {
+        this.nextNode = nextNode;
     }
 }
